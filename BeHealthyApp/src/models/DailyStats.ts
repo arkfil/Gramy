e81fe09880321e23e4e4a271279261c0a5943fc7
Agnieszka <agnieszka.szebla@gmail.com>
2018-04-26 23:10:17 +0200

    adding models

---
+++ BeHealthyApp/src/models/DailyStats.ts
@@ -0,0 +1,9 @@
+import {Pressure} from './Pressure';
+import{OtherSympthoms} from './OtherSympthoms';
+export class DailyStats {
+           measurement_id: string;
+        datetime: Date;
+        pressure: Pressure;
+        heart_rate: number;
+        other_sympthoms: OtherSympthoms;
+    }
