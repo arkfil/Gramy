a21f93c681e940041e9aec6b8a377a7a28719481
arkfil <arek0051@outlook.com>
2018-04-29 22:19:28 +0200

    Angular what a s...

--- BeHealthyApp/src/app/app-routing.module.ts
+++ BeHealthyApp/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [{path: 'login', component: LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent]
