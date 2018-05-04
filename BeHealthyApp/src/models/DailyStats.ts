058f66af1b8749dc596fad8915c07f70f7569af5
arkfil <arek0051@outlook.com>
2018-05-04 06:40:11 +0200

    clear garbage

--- BeHealthyApp/src/models/DailyStats.ts
+++
@@ -1,9 +0,0 @@
-import {Pressure} from './Pressure';
-import{OtherSympthoms} from './OtherSympthoms';
-export class DailyStats {
-        measurement_id: string;
-        datetime: Date;
-        pressure: Pressure;
-        heart_rate: number;
-        other_sympthoms: OtherSympthoms;
-    }
--- BeHealthyApp/src/models/OtherSympthoms.ts
+++
@@ -1,4 +0,0 @@
-export class OtherSympthoms {
-    headache_intensity: number;
-    other: Array<string>; //krwawienie z nosa, zaburzenia snu, zaczerwieniona twarz, zmęczenie
-}
