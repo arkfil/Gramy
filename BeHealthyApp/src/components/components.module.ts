0e2031cc0af3869990b424460f2f75089d093a4a
arkfil <arek0051@outlook.com>
2018-05-07 07:29:10 +0200

    progress with login

--- BeHealthyApp/src/components/components.module.ts
+++ BeHealthyApp/src/components/components.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { GoogleLoginComponent } from './google-login/google-login';
 
 @NgModule({
-	declarations: [GoogleLoginComponent],
+	declarations: [],
   imports: [CommonModule, // <--- for angular directives
             BrowserModule,
             IonicModule  // <--- for ionic components
   ],
-  exports: [GoogleLoginComponent],
+  exports: [],
 })
 export class ComponentsModule {}
--- BeHealthyApp/src/pages/login/login.ts
+++ BeHealthyApp/src/pages/login/login.ts
@@ -5,6 +5,7 @@ import { User } from '../../models/user';
 import { AngularFireAuth } from 'angularfire2/auth'
 import * as firebase from 'firebase/app';
 import { Facebook } from '@ionic-native/facebook';
+import { GooglePlus } from '@ionic-native/google-plus';
 
 
 
@@ -14,10 +15,13 @@ import { Facebook } from '@ionic-native/facebook';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  appUser: firebase.User;
   user= {} as User;
 
+
   constructor(private afAuth : AngularFireAuth, public navCtrl: NavController, public navParams: NavParams,
-    public loadingController: LoadingController, public platform: Platform, private facebook: Facebook) {
+    public loadingController: LoadingController, public platform: Platform, private facebook: Facebook,
+    private gplus: GooglePlus) {
   }
 
   ionViewDidLoad() {
@@ -25,14 +29,17 @@ export class LoginPage {
   }
 
   async loginWithEmailAndPassword(user: User){
-    let loading = this.loadingController.create({content : "Loading map ,please wait..."});
+    let loading = this.loadingController.create({content : "Loging in ,please wait..."});
     try{
       loading.present();
 
       const result = await this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
       console.log(result);
-      if(result)
+      this.appUser = result;
+      if(result){
         this.navCtrl.setRoot('MenuPage');
+      }
+
       loading.dismissAll();
     } catch(e){
       console.log("Error while signing in with email and password, error: " + e);
@@ -48,36 +55,92 @@ export class LoginPage {
 
 /* Login with facebook */
   loginWithFacebook(){
-    // try{
-      // if(this.platform.is('cordova')){
+    try{
+      if(this.platform.is('cordova')){
         this.facebook.login(["email"]).then(loginResp=>{
           let credential = firebase.auth.FacebookAuthProvider.credential(loginResp.authResponse.accessToken);
           firebase.auth().signInWithCredential(credential).then(info=>{
             console.log(info);
+            this.appUser = info;
           }).catch(e=>{
             console.log(e);
-
           });
-
         }).catch(e=>{
           console.log(e);
-
         });
 
-    //   }else{
-    //     let provider = new firebase.auth.FacebookAuthProvider();
-    //     firebase.auth().signInWithPopup(provider).then(()=>{
-    //       firebase.auth().getRedirectResult().then(result=>{
-    //         console.log(result);
-    //       }).catch(err=>{
-    //         console.log(err);
-    //       });
-    //     }).catch(e=>{});
-    //   }
-    // }catch(e){
+      }else{
+        let provider = new firebase.auth.FacebookAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(()=>{
+          firebase.auth().getRedirectResult().then(result=>{
+            console.log(result);
+            this.appUser=result;
+          }).catch(err=>{
+            console.log(err);
+          });
+        }).catch(e=>{});
+      }
+    }catch(e){
 
-    // }
+    }
   }
+
+  /* Login with google */
+  googleLogin() {
+    console.log("google login ");
+
+    if(this.platform.is('cordova')){
+
+      try{
+        //  const gplusUser =
+        this.gplus.login({
+          'webClientId':'756039335184-ke1ifc475v90oa1f52c66lb7d4tisbj6.apps.googleusercontent.com',
+          'offline': true,
+          'scopes': 'profile email'
+        }).then(loginResp=>{
+          console.log("here");
+
+          let credential = firebase.auth.GoogleAuthProvider.credential(loginResp.authResponse.idToken)
+          firebase.auth().signInWithCredential(credential).then(info=>{
+            console.log(info);
+            this.appUser = info;
+          }).catch(e=>{
+            console.log("ErrorHERE"+e);
+          });
+        }).catch(err=>{
+          console.log("Hell, here error: "+ err);
+        });
+
+      }catch(err){
+        console.log("Error while singing in with cordova google: " + err);
+      }
+    }else{
+      console.log("here");
+
+      try{
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        // const credential = this.afAuth.auth.signInWithPopup(provider);
+        firebase.auth().signInWithPopup(provider).then(()=>{
+          firebase.auth().getRedirectResult().then(result=>{
+            console.log(result);
+            this.appUser =result;
+          }).catch(err=>{
+            console.log(err);
+          });
+        }).catch(e=>{
+          console.log(e);
+        });
+
+
+      }catch(err){
+        console.log("Error while singing in with web google: " + err);
+
+      }
+    }
+  }
+
+
 
 
 }
