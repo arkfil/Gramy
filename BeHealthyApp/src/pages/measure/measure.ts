8dd65c613a1f57f162c49653f99d15002059d6ee
Artem <34673685+artemsln@users.noreply.github.com>
2018-05-18 22:23:59 +0200

    implemented ability to change emergency phone number in Profile. Added validation to call only correct phone number

--- BeHealthyApp/src/pages/measure/measure.ts
+++ BeHealthyApp/src/pages/measure/measure.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Modal,ModalOptions, ModalController} from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Modal, ModalOptions, ModalController, AlertController} from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { CardioParams } from '../../models/CardioParams';
 import { OtherSympthoms } from '../../models/OtherSympthoms';
 import { CallNumber } from '@ionic-native/call-number';
+import { Profile } from '../../models/profile';
+
 
 @IonicPage()
 @Component({
@@ -15,9 +17,13 @@ export class MeasurePage {
   userId: string;
   cardioParams: CardioParams;
   otherSympthoms: OtherSympthoms
+  telephoneNumber: string;
+  //telephoneNumberInt: number
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private modalCtrl: ModalController,
-    private afDatabase: AngularFireDatabase, private afAuth : AngularFireAuth, private callNumber: CallNumber) {
+    private afDatabase: AngularFireDatabase, private afAuth : AngularFireAuth, private callNumber: CallNumber,
+    private alertCtrl: AlertController) {
     try{
       this.afAuth.authState.subscribe(data => {
         if(data.email && data.uid){
@@ -28,25 +34,40 @@ export class MeasurePage {
         }
       });
       console.log('ionViewDidLoad Menu');
+
     }catch(e){
 
     }
   }
 
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter MeasurePage');
+    this.afAuth.authState.take(1).subscribe(auth => {
+      this.afDatabase.object(`profile/${auth.uid}`).valueChanges().take(1).subscribe(
+        data => {
+          if (data) {
+            console.log('GETTING VALUE OF PHONE NUMBER');
+            this.telephoneNumber = (<Profile>data).telephone;
+          }
+        });
+    });
+  }
+
+
   callEmergency(){
-    this.callNumber.callNumber(`*101#`, true);
-
-    // this.callNumber.isCallSupported()
-    // .then(function (response) {
-    //     if (response == true) {
-    //       this.callNumber.callNumber(`796284682`, true);
-
-    //     }
-    //     else {
-    //         // do something else
-    //     }
-    // });
-
+    // this.callNumber.callNumber(`*101#`, true);
+    //this.telephoneNumberInt = parseInt(this.telephoneNumber);
+    if (!(/[A-Za-z]/.test(this.telephoneNumber)))
+    {
+      this.callNumber.callNumber(this.telephoneNumber, true);
+    } else {
+      let alert = this.alertCtrl.create({
+        title: "Can't make a call!",
+        subTitle: "You have an invalid Emergeny phone number!",
+        buttons: ['OK']
+      });
+      alert.present();
+    }
   }
 
   ionViewDidLoad() {
